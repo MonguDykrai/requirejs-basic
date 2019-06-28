569e3dd66c6e7dc01a920262a1c4f0845ef4e864
M <M@qq.com>
2019-06-29 05:24:10 +0800

    9th commit

---
+++ js/main.5.js
@@ -0,0 +1,5 @@
+require(['calculator'], function (calculator) {
+  console.log(calculator)
+  console.log(calculator.add(1, 3))
+  console.log(calculator.sub(4, 5))
+})
--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-require(['calculator'], function (calculator) {
+require.config({
+  paths: {
+    'moment': 'https://cdn.bootcss.com/moment.js/2.24.0/moment'
+  }
+})
+
+require(['calculator', 'moment'], function (calculator, moment) {
   console.log(calculator)
   console.log(calculator.add(1, 3))
   console.log(calculator.sub(4, 5))
+
+  console.log(moment)
+
+  console.log('当前时间为：' + moment().format('YYYY-MM-DD HH:mm:ss'))
+  var x = 3;
+  var y = 4;
+  console.log('' + x + ' + ' + y + '的结果为：' + (x + y))
 })
