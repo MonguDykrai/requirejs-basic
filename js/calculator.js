cd6983993a81b925d7b3ad7c78939e5ba5b94403
M <M@qq.com>
2019-06-29 05:15:17 +0800

    8th commit

--- js/calculator.js
+++ js/calculator.js
@@ -1,10 +1,10 @@
-define(function () {
+define(['math'], function (math) {
   var add = function (x, y) {
-    return x + y
+    return math.add(x, y)
   }
 
   var sub = function (x, y) {
-    return x - y
+    return math.sub(x, y)
   }
 
   return {
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
-require(['math'], function (math) {
-  alert(math.add(1, 2))
+require(['calculator'], function (calculator) {
+  console.log(calculator)
+  console.log(calculator.add(1, 3))
+  console.log(calculator.sub(4, 5))
 })
